9ad431d86b995f613da80b5013c755a6212f9941
Evgeny Yusov <genyok@gmail.com>
2022-11-19 20:10:24 -0800

    add search result items

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,29 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Feather';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Searchbar } from 'react-native-paper';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+import _ from 'lodash';
 
 const styles = StyleSheet.create({
   input: {
@@ -32,7 +15,60 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  medicineItem: {
+    width: "95%",
+    marginTop: 6,
+    marginBottom: 6,
+    marginLeft: 10,
+    padding: 5,
+    borderRadius: 8,
+    elevation: 5,
+    shadowColor: "rgba(0,0,0,0.4)",
+    backgroundColor: "white",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+  containerView: {
+    alignItems: 'center',
+    flexDirection: "row",
+    margin: 10,
+    flexWrap: "wrap",
+  },
+  medicineItemRightPart: {
+    margin: 10,
+  },
+  availabiltyText: {
+    backgroundColor: 'green',
+    color: "white",
+    alignSelf: 'flex-start',
+    borderRadius: 6,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 4,
+    paddingRight: 4,
+    fontSize: 13,
+  }
 });
+
+const MedicineItem = () => {
+  return (
+    <View style={styles.medicineItem}>
+      <View style={styles.containerView}>
+        <Image
+          style={styles.df}
+          source={require("./assets/favicon.png")}
+        />
+        <View style={styles.medicineItemRightPart}>
+          <Text style={{ fontSize: 17 }}>Escitalopram Cinfa 10mg</Text>
+          <View style={{ marginTop: 3 }}>
+            <Text style={styles.availabiltyText}>AVAILABLE</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
 
 const SearchComponent = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
@@ -46,6 +82,9 @@ const SearchComponent = () => {
         onChangeText={onChangeSearch}
         value={searchQuery}
       />
+      <ScrollView>
+        { _.range(10).map(i => <MedicineItem key={i}/>) }
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -62,7 +101,7 @@ function HomeScreen() {
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Saved section is not implemented yet :(</Text>
     </View>
   );
 }
